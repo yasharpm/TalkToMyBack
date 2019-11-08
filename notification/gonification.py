ab2dfd7aa548ca5703ab8550629c19554a607272
Yashar <yashar.p@780.ir>
2019-11-08 13:31:12 +0330

    Testing fcm push

--- notification/gonification.py
+++ notification/gonification.py
@@ -1,6 +1,7 @@
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import messaging
+import json
 
 from entity.user.user import User
 
@@ -16,8 +17,9 @@ def send_notification(user, sync):
     if not fcm_token:
         return
 
-    data = sync.get_public_obj()
+    data = {'sync': json.dumps(sync.get_public_obj())}
 
     notification = messaging.Notification(title='salam', body='this is the body', image=None)
     message = messaging.Message(data=data, token=fcm_token, notification=notification)
     response = messaging.send(message)
+
