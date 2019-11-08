a0d000d8dd0e7edcee762807393a1bfa47d640f2
Yashar <yashar.p@780.ir>
2019-11-08 15:56:04 +0330

    Testing fcm push

--- notification/gonification.py
+++ notification/gonification.py
@@ -21,6 +21,4 @@ def send_notification(user, sync):
 
     notification = messaging.Notification(title='salam', body='this is the body', image=None)
     message = messaging.Message(data=data, token=fcm_token, notification=notification)
-    response = messaging.send(message)
-
-    print('push response:', response)
+    messaging.send(message)
