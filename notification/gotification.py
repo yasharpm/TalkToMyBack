e461b7bd01a805fbaf69e0b0bf03c4b60b8bfd9d
Yashar PourMohammad <yasharpm@users.noreply.github.com>
2020-07-30 02:15:24 +0430

    Update gotification.py
    Relative address for firebase key

--- notification/gotification.py
+++ notification/gotification.py
@@ -9,7 +9,7 @@ from entity.user.user import User
 
 
 def init():
-    cred = credentials.Certificate("/home/talktome/opentalkz-firebase-adminsdk.json")
+    cred = credentials.Certificate("../../opentalkz-firebase-adminsdk.json")
     firebase_admin.initialize_app(cred)
 
 
