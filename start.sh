6a1a842c1d84f97e694945e83c9bea1af73f8e7c
Yashar PourMohammad <yasharpm@gmail.com>
2020-08-04 23:54:49 +0430

    Start and Stop scripts updated

--- start.sh
+++ start.sh
@@ -2,9 +2,9 @@ echo starting
 
 # nohup /usr/local/bin/python3.7 talktomyback.py > myback.log &1>2 &
 
-cd /home/talktome/Python-3.7.0
-nohup ./python /home/talktome/talktomyback/talktomyback.py > myback.log &
+# cd /home/talktome/Python-3.7.0
+nohup python3.8 /home/opentalkz/api/talktomyback/talktomyback.py > myback.log &
 
-echo $! > /home/talktome/talktomyback/myback_pid.txt
+echo $! > /home/opentalkz/api//talktomyback/myback_pid.txt
 
 echo started
--- stop.sh
+++ stop.sh
@@ -1,6 +1,6 @@
 echo stopping
 
-kill -9 `cat /home/talktome/talktomyback/myback_pid.txt`
-rm -f /home/talktome/talktomyback/myback_pid.txt
+kill -9 `cat /home/opentalkz/api//talktomyback/myback_pid.txt`
+rm -f /home/opentalkz/api/talktomyback/myback_pid.txt
 
 echo stopped
