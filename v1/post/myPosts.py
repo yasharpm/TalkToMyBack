10e58bc68d4a4a089fa62e38d33ca48721015414
Yashar <yashar.p@780.ir>
2020-08-09 10:41:26 +0430

    Ooops!

--- v1/post/myPosts.py
+++ v1/post/myPosts.py
@@ -18,7 +18,7 @@ class myPosts:
         count = req.get_param_as_int('count', min_value=1, max_value=MAXIMUM_COUNT, default=10)
         offset = req.get_param_as_int('offset', min_value=0, default=0)
 
-        user_obj = user.get_public_obj()
+        user_obj = user.get_obj()
 
         total_count = user_obj[User.POSTED_POSTS_COUNT]
 
