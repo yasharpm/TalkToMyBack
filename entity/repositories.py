749253ac20365a00283356acee3a02faf3b2638d
Yashar <yashar.p@780.ir>
2020-08-09 10:15:09 +0430

    Dependency interdependency fixed

--- entity/repositories.py
+++ entity/repositories.py
@@ -1,11 +1,14 @@
-from entity.token.token_repo import TokenRepo
-from entity.refresh_token.refresh_token_repo import RefreshTokenRepo
-from entity.user.user_repo import UserRepo
 from entity.post.post_repo import PostRepo
-from entity.sync.sync_repo import SyncRepo
-
-TOKEN_REPO = TokenRepo()
-REFRESH_TOKEN_REPO = RefreshTokenRepo()
-USER_REPO = UserRepo()
 POST_REPO = PostRepo()
+
+from entity.user.user_repo import UserRepo
+USER_REPO = UserRepo()
+
+from entity.token.token_repo import TokenRepo
+TOKEN_REPO = TokenRepo()
+
+from entity.refresh_token.refresh_token_repo import RefreshTokenRepo
+REFRESH_TOKEN_REPO = RefreshTokenRepo()
+
+from entity.sync.sync_repo import SyncRepo
 SYNC_REPO = SyncRepo()
